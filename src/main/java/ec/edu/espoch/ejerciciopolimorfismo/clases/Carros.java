7b8608c125c7a3d61847a02bc6374214681cb5d5
JIrleidySantos <jirleidysantosm@gmail.com>
2025-01-09 23:01:09 -0500

    Cambio

---
+++ src/main/java/ec/edu/espoch/ejerciciopolimorfismo/clases/Carros.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ec.edu.espoch.ejerciciopolimorfismo.clases;
+
+/**
+ *
+ * @author mundo
+ */
+public class Carros extends Vehiculos{
+    
+    @Override
+    public String transportar(){
+    return "El carro Transportara productos por carretera de forma eficiente. ";
+    }
+}
